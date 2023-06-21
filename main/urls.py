e73f2d2ce063aada067a400f0ee56dd0447fa341
DiegoSanchez <diegosanchez1234567@gmai.com>
2023-06-20 23:44:58 -0400

    arreglos static

--- main/urls.py
+++ main/urls.py
@@ -5,16 +5,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('main/ClassicGames.html', views.classicgames, name='classicgames'),
-    path('main/ClassicGames2.html', views.classicgames2, name='classicgames2'),
-    path('main/Contactanos.html', views.contactanos, name='contactanos'),
-    path('main/IniciarSesion.html', views.iniciarsesion, name='iniciarsesion'),
-    path('main/NewGames.html', views.newgames, name='newgames'),
-    path('main/NewGames2.html', views.newgames2, name='newgames2'),
-    path('main/Nosotros.html', views.nosotros, name='nosotros'),
-    path('main/Registrarse.html', views.registrarse, name='registrarse'),
-    path('main/RetroGames.html', views.retrogames, name='retrogames'),
-    path('main/RetroGames2.html', views.retrogames2, name='retrogames2'),
+    path('classicgames', views.classicgames, name='classicgames'),
+    path('classicgames2', views.classicgames2, name='classicgames2'),
+    path('contactanos', views.contactanos, name='contactanos'),
+    path('iniciarsesion', views.iniciarsesion, name='iniciarsesion'),
+    path('newgames', views.newgames, name='newgames'),
+    path('newgames2', views.newgames2, name='newgames2'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('registrarse', views.registrarse, name='registrarse'),
+    path('retrogames', views.retrogames, name='retrogames'),
+    path('retrogames2', views.retrogames2, name='retrogames2'),
     
 
     
