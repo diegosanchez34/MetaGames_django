d65738aee5ca073a94cff5d59912b4bdac03f48b
nicolascarcamo <nic.carcamo@duocuc.cl>
2023-06-20 23:01:57 -0400

    urls y statics ekisde

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,17 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('main/ClassicGames.html', views.classicgames, name='classicgames'),
+    path('main/ClassicGames2.html', views.classicgames2, name='classicgames2'),
+    path('main/Contactanos.html', views.contactanos, name='contactanos'),
+    path('main/IniciarSesion.html', views.iniciarsesion, name='iniciarsesion'),
+    path('main/NewGames.html', views.newgames, name='newgames'),
+    path('main/NewGames2.html', views.newgames2, name='newgames2'),
+    path('main/Nosotros.html', views.nosotros, name='nosotros'),
+    path('main/Registrarse.html', views.registrarse, name='registrarse'),
+    path('main/RetroGames.html', views.retrogames, name='retrogames'),
+    path('main/RetroGames2.html', views.retrogames2, name='retrogames2'),
+    
+
+    
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,44 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'main/index.html', context)
+
+def classicgames(request):
+    context={}
+    return render(request, 'main/ClassicGames.html', context)
+
+def classicgames2(request):
+    context={}
+    return render(request, 'main/ClassicGames2.html', context)
+
+def contactanos(request):
+    context={}
+    return render(request, 'main/Contactanos.html', context)
+
+def iniciarsesion(request):
+    context={}
+    return render(request, 'main/IniciarSesion.html', context)
+
+def newgames(request):
+    context={}
+    return render(request, 'main/NewGames.html', context)
+
+def newgames2(request):
+    context={}
+    return render(request, 'main/NewGames2.html', context)
+    
+def nosotros(request):
+    context={}
+    return render(request, 'main/Nosotros.html', context)
+
+def registrarse(request):
+    context={}
+    return render(request, 'main/Registrarse.html', context)
+
+def retrogames(request):
+    context={}
+    return render(request, 'main/RetroGames.html', context)
+
+def retrogames2(request):
+    context={}
+    return render(request, 'main/RetroGames2.html', context)
+    
